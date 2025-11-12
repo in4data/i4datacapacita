e815a0505a4131691f63e0e44c79ac52b188c46c
Iredefbmac_14 <iredefbmac_14@Mac-mini-de-Iredefbmac-14.local>
2025-11-12 13:44:12 -0300

    build: project initialization commit

---
+++ i4data/i4data/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  i4data
+//
+//  Created by Iredefbmac_14 on 12/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ i4data/i4data/i4dataApp.swift
@@ -0,0 +1,17 @@
+//
+//  i4dataApp.swift
+//  i4data
+//
+//  Created by Iredefbmac_14 on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct i4dataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
