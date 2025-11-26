9ee0344d79bb8da999e430574e3cf230d08ae8f9
Angelo Matos <angelomatosn@gmail.com>
2025-11-26 16:29:18 -0300

    Tab Bar com design correspondente ao Figma
    Foram criadas: TabBar (adicionada à pasta components junto do botão reutilizável), views auxiliares para a TabBar, que possui botões funcionais e escaláveis. Foi adicionado um novo color set: "Background-Tab" específico para a Tab Bar.

---
+++ i4data/i4data/NotificationView.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileView.swift
+//  i4data
+//
+//  Created by iredefbmac_01 on 26/11/25.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Notificações")
+            .navigationTitle("Notificações")
+    }
+}
---
+++ i4data/i4data/SearchView.swift
@@ -0,0 +1,15 @@
+//
+//  SearchView.swift
+//  i4data
+//
+//  Created by iredefbmac_01 on 26/11/25.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text("Buscar")
+            .navigationTitle("Buscar")
+    }
+}
